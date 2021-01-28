b6666f3e0177bc652d6565cc6a1386b518696330
Gazda99 <jakub.gazdzik@outlook.com>
2021-01-28 21:52:04 +0100

    Update ISolution.cs

--- AoCBase/ISolution.cs
+++ AoCBase/ISolution.cs
@@ -6,8 +6,27 @@
 /// <typeparam name="T1">Part one return type, conversion to string must be possible</typeparam>
 /// <typeparam name="T2">Part two return type, conversion to string must be possible</typeparam>
 public interface ISolution<T0, T1, T2> {
+    /// <summary>
+    /// Stores the data
+    /// </summary>
+    public T0 Data { get; set; }
+
+    /// <summary>
+    /// Returns the read data from file
+    /// </summary>
+    /// <returns></returns>
     public T0 ReadData();
+
+    /// <summary>
+    /// Calculates answer for task part one 
+    /// </summary>
+    /// <returns></returns>
     public T1 SolveFirst();
+
+    /// <summary>
+    ///  Calculates answer for task part two 
+    /// </summary>
+    /// <returns></returns>
     public T2 SolveSecond();
 }
 }
