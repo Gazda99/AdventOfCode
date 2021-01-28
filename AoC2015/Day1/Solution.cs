352d119cb0a0ebe1170507c4edf6ce09791caeb6
Gazda99 <jakub.gazdzik@outlook.com>
2021-01-28 17:42:58 +0100

    Day 1 start

---
+++ AoC2015/Day1/Solution.cs
@@ -0,0 +1,13 @@
+﻿using AoCBase;
+
+namespace AoC2015.Day1 {
+class Solution : ISolution<int, int> {
+    public int SolveFirst() {
+        throw new System.NotImplementedException();
+    }
+
+    public int SolveSecond() {
+        throw new System.NotImplementedException();
+    }
+}
+}
