829ffbfa09ed66277d8f20ccdf87e66f1eb1b1aa
Gazda99 <jakub.gazdzik@outlook.com>
2021-01-28 22:01:29 +0100

    User interface for AoC2015

--- AoC2015/Program.cs
+++ AoC2015/Program.cs
@@ -1,5 +1,8 @@
 ﻿namespace AoC2015 {
 class Program {
-    static void Main(string[] args) { }
+    static void Main(string[] args) {
+        UserInterface ui = new UserInterface();
+        ui.Start();
+    }
 }
 }
---
+++ AoC2015/UserInterface.cs
@@ -0,0 +1,19 @@
+﻿using AoCBase;
+
+namespace AoC2015 {
+class UserInterface : BaseUserInterface {
+    /// <summary>
+    /// Prints the answer for part one and two for day specified in param
+    /// </summary>
+    /// <param name="dayNumber">Number of day</param>
+    public override void ShowAnswer(int dayNumber) {
+        IAnswer iAnswer = dayNumber switch {
+            1 => new Day1.Solution(dayNumber),
+            2 => new Day2.Solution(dayNumber),
+            _ => null
+        };
+
+        WriteAnswer(iAnswer, dayNumber);
+    }
+}
+}
